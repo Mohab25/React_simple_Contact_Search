3874adadef4ece255fa623993142c065c9b976b6
mohab <homab3@gmail.com>
2020-07-17 13:21:22 +0200

    Updates

---
+++ precache-manifest.c3ae3176a1c9527539f96c7dd4a5d65a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93530b30699d6c81c21d8f974d319c98",
+    "url": "/React_simple_Contact_Search/index.html"
+  },
+  {
+    "revision": "cfa9bafc096fe98ddcfc",
+    "url": "/React_simple_Contact_Search/static/css/main.96d05412.chunk.css"
+  },
+  {
+    "revision": "d36ddeed411276b0e42c",
+    "url": "/React_simple_Contact_Search/static/js/2.5da1b3fa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React_simple_Contact_Search/static/js/2.5da1b3fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfa9bafc096fe98ddcfc",
+    "url": "/React_simple_Contact_Search/static/js/main.eccc35c3.chunk.js"
+  },
+  {
+    "revision": "66dcefb01bb872d8e23f",
+    "url": "/React_simple_Contact_Search/static/js/runtime-main.4db8cf92.js"
+  }
+]);
